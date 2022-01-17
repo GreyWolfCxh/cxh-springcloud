c9830614fb107cd6a2f0c95ca2b7e25e1d3d7244
GreyWolfCxh <1278826642@qq.com>
2022-01-17 11:46:42 +0800

    springcloud整合Sentinel使用Nacos存储规则

--- sentinel/src/main/java/com/cxh/sentinel/SentinelApplication.java
+++ sentinel/src/main/java/com/cxh/sentinel/SentinelApplication.java
@@ -3,11 +3,16 @@ package com.cxh.sentinel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @SpringBootApplication
 public class SentinelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SentinelApplication.class, args);
 	}
+
 
 }
